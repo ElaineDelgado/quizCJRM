24ee129c47b59747a04ca3019ae0b97acfd79a78
Elaine Delgado <delgado.elaine03@gmail.com>
2021-10-25 18:25:19 -0300

    adicionando opçoes, add setInterval para exibicao de pontos, corrigindo values e posicoes das opcoes corretas

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 const form = document.querySelector('form.quiz-form')
-const paragraphQuizAnswer = document.createElement('p')
-const btnVoltarInicio = document.querySelector('.btn-voltar')
+const finalResult = document.querySelector('div.result')
 
-const correctAnswers = ['3', '3', '3', '3', '3', '3', '3', '3', '3', '3']
+
+const correctAnswers = ['2', '4', '3', '1', '3', '2', '4', '1', '3', '2']
+
 let totalScore = 0
 
 const totalScorePoints = (event) => {
-  event.preventDefault()
-  
+  event.preventDefault()  
 
   const userAnswers = [
     form.anime1.value,
@@ -28,8 +28,31 @@ const totalScorePoints = (event) => {
     }
   })
 
-  btnVoltarInicio.insertAdjacentElement('beforebegin', paragraphQuizAnswer)
-  paragraphQuizAnswer.textContent = ` Você fez ${totalScore} pontos de 100!`
+  scrollTo(0, 0) //rola a pagina para o eixo x e y,ambos posicao zero
+  
+  finalResult.classList.remove('d-none')
+
+  /**
+   * a quantidade de vezes q a funcao de callback inserida no setInterval vai ser executada
+   * vai ser a mesma do counter, ou seja, se o counter final for 50, a funcao do setInterval 
+   * vai ser executada 50 vezes pra q a nimacao dos numeros aconteça
+   */
+  let counter = 0
+
+  const timer = setInterval(() => {
+    if (counter === totalScore) {
+      clearInterval(timer)
+    }
+    finalResult.querySelector('span').textContent = `${counter}%`
+    counter++
+  }, 10)
+  console.log(counter)
 }
 
-form.addEventListener('submit', totalScorePoints)
+form.addEventListener('submit', totalScorePoints) // acertando todas o resutado é 99%
+
+
+
+// a funcao setInterval gera um id q está armazenado na const timer
+// console.log(timer)  
+//1 , o id setInterval , então passamos a timer no argumento do clearInterval
